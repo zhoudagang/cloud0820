05fb3c1e52b4785bf3e048547f539b1aafe35110
zhoudagang <15371646664@163.com>
2020-08-21 17:38:29 +0800

    add gateway

---
+++ cloud-gateway/src/main/java/com/everyman/cloud/config/GatewayConfig.java
@@ -0,0 +1,13 @@
+package com.everyman.cloud.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zhougang
+ * @date 2020/08/21
+ */
+@Configuration
+public class GatewayConfig {
+
+
+}
